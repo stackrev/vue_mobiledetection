ea5ad81382b7dfc48dce353d89c7668974ad5733
stackrev <farbitherast5888@gmail.com>
2022-08-24 15:45:05 +0200

    start

---
+++ app/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+<div id="app">
+  <IsThisMobile/>
+</div>
+</template>
+
+<script>
+import IsThisMobile from './components/IsThisMobile.vue'
+
+export default {
+  name: 'App',
+  components: {
+    IsThisMobile
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ app/src/components/IsThisMobile.vue
@@ -0,0 +1,74 @@
+<template>
+  <div id="IsThisMobile">
+    <p>
+      We will identify if it's a mobile by parsing the user agent string defined as a standards in HTTP <a href="https://datatracker.ietf.org/doc/html/rfc7231#section-5.5.3">here</a> in RFC7231.
+    </p>
+    <p><b>User Agent string:</b> <i>{{userAgent}}</i></p>
+    <p>Checking mobile through the metadata: 
+      <ul>
+        <li>vendor: {{vendor}}</li>
+        <li>opera: {{opera}}</li>
+        <div v-if="uaParsed !== null">
+          <li>Browser: {{uaParsed.getBrowser()}}</li>
+          <li>Device: {{uaParsed.getDevice()}}</li>
+          <li>Engine: {{uaParsed.getEngine()}}</li>
+          <li>OS: {{uaParsed.getOS()}}</li>
+          <li>CPU: {{uaParsed.getCPU()}}</li>
+        </div>
+      </ul>
+    </p>
+    <div v-if="!isMobile">desktop</div>
+    <div v-else>mobile</div>
+  </div>
+</template>
+
+<script>
+import UAParser from 'ua-parser-js';
+
+/**
+ * This app provides information of the browser's host.
+ */
+export default {
+  name: "IsThisMobile",
+  data() {
+    return {
+      isMobile: true,
+      userAgent: "",
+      vendor: "",
+      opera: "",
+      uaParsed: null,
+    };
+  },
+
+  
+  mounted() {
+    this.loadBrowserHostData();
+  },
+
+  methods: {
+    loadBrowserHostData() {
+      // 1. Simplest way to ID a mobile device.
+      // window.innerWidth < 600;
+      // 2. The more sophisticated way is through the user agent (UA) string
+      // This string is in the form: User-Agent = product *( RWS ( product / comment ) )
+      // 3. https://www.npmjs.com/package/ua-parser-js will structure this info.
+
+      this.userAgent= navigator?.userAgent || "NA";
+      this.vendor= navigator?.vendor || "NA";
+      this.opera = navigator?.opera || "NA";
+      if (navigator?.userAgent != null) {
+        this.uaParsed = new UAParser(navigator.userAgent);
+      }
+    }
+  },
+};
+
+</script>
+
+<style>
+#IsThisMobile {
+  text-align: left;
+  color: #14181c;
+  margin: 15px;
+}
+</style>
